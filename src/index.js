5dad89947f56a765748c7c30176b3a41bb109968
Sergii Podobrii <podobri@ukr.net>
2023-09-03 17:13:59 +0300

    adding read

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   //   <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/team-project-frontend">
+      <BrowserRouter basename="/YourPet">
         <ThemeProvider theme={theme}>
           <Global styles={GlobalStyles} />
           <App />
